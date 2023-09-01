01612f628bc2471de9fa004ab9ca286416667ba2
Amirkhan <amirhan75@gmail.com>
2023-09-01 15:35:59 +0600

    food 0.1

--- src/Main.go
+++ src/Main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"github.com/BurntSushi/toml"
+	"log"
+	"net/http"
+)
+
+type сonfig struct {
+	ServerPort string `toml:"serverPort"`
+}
+
+func handleNewsLetter(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+}
+
 func main() {
+	cfg := &сonfig{}
+	_, err := toml.DecodeFile("src/config/config.toml", cfg)
+	if err != nil {
+		log.Fatalf("Ошибка декодирования файла конфигов %v", err)
+	}
+
+	println(cfg.ServerPort)
+
+	http.HandleFunc("/health", handleNewsLetter)
+	log.Fatal(http.ListenAndServe(cfg.ServerPort, nil))
 }
